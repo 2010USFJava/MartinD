1915ec9319cde2c5f28c01ba136d4a12a8feb266
Dez <dezmaniquemartin6@gmail.com>
2020-10-27 14:11:11 -0400

    made driver class w/scanner and AccountInfo class

---
+++ BankApp/src/main/java/com/revature/driver/AccountInfo.java
@@ -0,0 +1,25 @@
+package com.revature.driver;
+
+public class AccountInfo {
+	
+	String name;
+	String address;
+	int phoneNumber;
+	String emailAddress;
+	int dateOfBirth;
+	String accountType;
+	int accountNumber;
+	
+	
+	AccountInfo(String name, String address, int phoneNumber, String emailAddress, int dateOfBirth, String accountType) {
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.emailAddress = emailAddress;
+		this.dateOfBirth = dateOfBirth;
+		this.accountType = accountType;
+	}
+	
+	
+
+}
